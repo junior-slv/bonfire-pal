6bf72a2f510ae28c81cfcc5d0dd7e97c9ca910fa
Junior <dirceusilva0422@gmail.com>
2022-12-12 23:19:44 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import './app.css';
+
+import darksouls from './assets/darksouls.jpg';
+import darksouls2 from './assets/darksouls2.jpg';
+import darksouls3 from './assets/darksouls3.jpg';
+
+function App() {
+    return ( 
+    <div className="container">
+        <div className="menu">
+            <div className="up-menu">
+                <h1>Select your game!</h1>
+            </div>
+            <div className="bottom-menu">
+                <span><img src={darksouls} alt="" /></span>
+                <span><img src={darksouls2} alt="" /></span>
+                <span><img src={darksouls3} alt="" /></span>
+            </div>
+        </div>
+    </div> );
+}
+
+export default App;
